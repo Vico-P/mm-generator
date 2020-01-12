49232e17dd578329704652ba4fcf260c9d4056fb
Vico <victor1.pancrate@epitech.eu>
2020-01-12 19:35:36 +0100

    Table template is now in a hook state object containing all the fields

--- src/Modules/PostGeneratorModule/PostGenerator.js
+++ src/Modules/PostGeneratorModule/PostGenerator.js
@@ -2,14 +2,37 @@ import React, { useState } from 'react';
 // import { useSelector } from 'react-redux' uncomment when redux-store is ready
 
 function PostGenerator() {
-    const [arrayPost, setArrayPost] = useState([]);
+    const [arrayPost, setArrayPost] = useState({
+      urlTimestamp: '',
+      urlName: '',
+      images: [],
+      message: '',
+    });
     const rows = [];
     const handleChange = (newValue, field, index) => {
-      let newArray = [...arrayPost];
-      newArray[index][field] = newValue;
-      setArrayPost(newArray);
-    }
-    arrayPost.forEach((row) => {
+      if (index || index === 0) {
+        let newArray = [...arrayPost.images];
+        newArray[index][field] = newValue;
+        arrayPost.images = newArray;
+      } else {
+        arrayPost[field] = newValue;
+      }
+      setArrayPost(Object.assign({}, arrayPost));
+    };
+
+    const addNewLine = () => {
+        arrayPost.images = [...arrayPost.images, {
+          index: arrayPost.images.length,
+          name: '',
+          imgUrl : '',
+          imgName : '',
+          desc: '',
+          price: 12
+        }];
+        setArrayPost(Object.assign({}, arrayPost));
+    };
+
+    arrayPost.images.forEach((row) => {
       rows.push(
         <tr key={row.index}>
           <td>
@@ -36,25 +59,23 @@ function PostGenerator() {
     });
     return (
       <div>
+      <span>Timestamp global</span><input type="text" value={arrayPost.urlTimestamp} onChange={(event) => {handleChange(event.target.value, "urlTimestamp")}}/>
+      <span>Name for url</span><input type="text" value={arrayPost.urlName} onChange={(event) => {handleChange(event.target.value, "urlName")}}/>
       <table>
         <tbody>
             {rows}
         </tbody>
       </table>
-      <button onClick={() => {
-        setArrayPost([...arrayPost, {
-          index: arrayPost.length,
-          name: '',
-          imgUrl : '',
-          imgName : '',
-          desc: '',
-          price: 12
-        }]);
-      }}>
+      <div>
+      <textarea value={arrayPost.message} onChange={(event) => {handleChange(event.target.value, "message")}}
+      rows="5" cols="33" placeholder="If you have more to say...">
+      </textarea>
+      </div>
+      <button onClick={() => {addNewLine()}}>
         Ajouter une ligne
       </button>
       <button onClick={() => console.log(arrayPost)}>
-        Voir le contenu du tableau
+        Voir le contenu du modèle
       </button>
       </div>
     );
